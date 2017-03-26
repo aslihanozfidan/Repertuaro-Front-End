a052da50afe2233f61635589b0856724dc9d6566
Aslıhan Özfidan <aslihanozfidan@gmail.com>
2017-03-26 15:04:43 +0300

    Bugs fixed about repertuar

--- src/app/+repertuar/repertuar_ekle/repertuar_ekle.component.ts
+++ src/app/+repertuar/repertuar_ekle/repertuar_ekle.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+
 // import 'rxjs/add/operator/map';
 // import 'rxjs/add/operator/catch';
 
@@ -8,17 +9,17 @@ import { SharedComponent } from './../../shared/shared.component';
 import { UsersService } from './../../services/users.service';
 import { user } from './../../model/user';
 
+import { RepertuarModule } from '../repertuar.module';
 
 @Component({
-    selector: 'repertuar',
-    templateUrl: './repertuar.component.html',
-    styleUrls: ['./repertuar.component.scss'],
+    selector: 'repertuar_ekle',
+    templateUrl: './repertuar_ekle.component.html',
+    styleUrls: ['./repertuar_ekle.component.scss'],
     providers: [UsersService]
 })
 
-export class RepertuarComponent implements OnInit, OnDestroy {
-    repertuarName="mysongs";
-    repertuarEklePageUrl="/repertuarlar/repertuar_ekle";
+export class RepertuarEkleComponent implements OnInit, OnDestroy {
+      
     private commentsUrl = 'api';
     private users: user[];
     constructor(
@@ -31,7 +32,7 @@ export class RepertuarComponent implements OnInit, OnDestroy {
     }
 
     ngOnInit() {
-        this.route.params.subscribe(key => this.repertuarName = key["repertuarName"]);
+        
     }
 
 
